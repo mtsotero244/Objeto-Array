55fe5b94382e8866711e6db8a33e01c2d45f58c3
Julia <0001442875@senaimgaluno.com.br>
2025-08-05 08:58:54 -0300

    projetoarray

---
+++ objetoarray.js
@@ -0,0 +1,6 @@
+// Criando um objeto chamado "pessoa"
+const pessoa = {
+  nome: "Lucas", // Propriedade: nome (chave), com valor "Lucas"
+  idade: 30, // Propriedade: idade (chave), com valor 30
+  profissao: "Engenheiro", // Propriedade: profissao (chave), com valor "Engenheiro"
+};
