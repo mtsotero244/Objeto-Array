eaa099ebade9bda3967b26576d801c1e54986da5
Julia <0001442875@senaimgaluno.com.br>
2025-08-05 09:32:23 -0300

    acesso aos dados

---
+++ acessoaosdados.js
@@ -0,0 +1,8 @@
+const pessoa = {
+  nome: "Lucas",
+  idade: 30,
+  profissao: "Engenheiro",
+};
+console.log(pessoa.nome); // Lucas
+console.log(pessoa.idade); // 30
+console.log(pessoa.profissao); // Engenheiro
