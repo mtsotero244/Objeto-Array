f399b18e72fb0e4f745f93fccf0885e0ce7e5cd3
Julia <0001442875@senaimgaluno.com.br>
2025-08-05 09:17:09 -0300

    Variáveis

---
+++ variaveis.js
@@ -0,0 +1,11 @@
+// 3 variáveis separadas:
+const nome = "Lucas";
+const idade = 30;
+const profissao = "Engenheiro";
+
+// variáveis juntas em único objeto:
+const pessoa = {
+  nome: "Lucas",
+  idade: 30,
+  profissao: "Engenheiro",
+};
